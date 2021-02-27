1b269dbcc2a11d1ff4f16c02cba4db97e770d19d
Naveed Naqi <nnaqi5534@gmail.com>
2021-02-27 12:28:41 -0500

    updated register frontend

--- client/src/components/auth/RegisterView.js
+++ client/src/components/auth/RegisterView.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
-import Container from "react-bootstrap/Container";
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
 const RegisterView = (props) => {
   const [state, setState] = useState({
@@ -25,50 +30,101 @@ const RegisterView = (props) => {
   };
 
   return (
-    <Container>
-      <h1>Register</h1>
-      <br></br>
-      <Form onSubmit={registerUser} autoComplete="off">
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            onChange={handleChange}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <Grid container justify="center" direction="column" alignItems="center">
+      <Grid item>
+        <h1>LOGO</h1>
+      </Grid>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={handleChange}
-          />
-        </Form.Group>
+      <Card style={{ width: "60%" }}>
+        <CardHeader title="Register"></CardHeader>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={handleChange}
-          />
-        </Form.Group>
+        <CardContent>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item>
+              <TextField variant="outlined" label="Username" id="username" />
+            </Grid>
+            <Grid item>
+              <TextField variant="outlined" label="Email" id="email" />
+            </Grid>
+            <Grid item>
+              <TextField variant="outlined" label="Password" id="password" />
+            </Grid>
+            <Grid item>
+              <TextField
+                variant="outlined"
+                label="Confirm Password"
+                id="confirmPassword"
+              />
+            </Grid>
 
-        <Link to="/">Already have an account?</Link>
+            <Grid item>
+              <Button
+                onClick={registerUser}
+                variant="contained"
+                color="primary"
+              >
+                Register
+              </Button>
+            </Grid>
+          </Grid>
 
-        <br></br>
-        <br></br>
+          <br></br>
+          <div>
+            <Link to="/">Already have an account?</Link>
+          </div>
+        </CardContent>
+      </Card>
+    </Grid>
+    // <Container>
+    //   <h1>Register</h1>
+    //   <br></br>
+    //   <Form onSubmit={registerUser} autoComplete="off">
+    //     <Form.Group controlId="formBasicEmail">
+    //       <Form.Label>Email address</Form.Label>
+    //       <Form.Control
+    //         type="email"
+    //         placeholder="Enter email"
+    //         onChange={handleChange}
+    //       />
+    //       <Form.Text className="text-muted">
+    //         We'll never share your email with anyone else.
+    //       </Form.Text>
+    //     </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-      </Form>
-    </Container>
+    //     <Form.Group controlId="formBasicPassword">
+    //       <Form.Label>Password</Form.Label>
+    //       <Form.Control
+    //         type="password"
+    //         placeholder="Password"
+    //         onChange={handleChange}
+    //       />
+    //     </Form.Group>
+
+    //     <Form.Group controlId="formBasicPassword">
+    //       <Form.Label>Confirm Password</Form.Label>
+    //       <Form.Control
+    //         type="password"
+    //         placeholder="Password"
+    //         onChange={handleChange}
+    //       />
+    //     </Form.Group>
+
+    //     <Link to="/">Already have an account?</Link>
+
+    //     <br></br>
+    //     <br></br>
+
+    //     <Button variant="primary" type="submit">
+    //       Register
+    //     </Button>
+    //   </Form>
+    // </Container>
   );
 };
 
