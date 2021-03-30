09163f12c69fb81b0ac59fedb11d1b96dc2f0d0f
Naveed Naqi <nnaqi5534@gmail.com>
2021-03-30 14:37:46 -0400

    added some middleware for checkignauthentication

--- routes/like.js
+++ routes/like.js
@@ -21,55 +21,47 @@ router.get("/", async (req, res, next) => {
 });
 
 router.delete("/remove", async (req, res, next) => {
-    try {
-      const { id } = req.query;
-      console.log(id);
-  
-      const findLike = await Like.findOne({
+  try {
+    const { id } = req.query;
+    console.log(id);
+
+    const findLike = await Like.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (findLike !== null) {
+      const likes = await Like.destroy({
         where: {
           id: id,
         },
       });
-  
-      if (findLike !== null) {
-        const likes = await Like.destroy({
-          where: {
-            id: id,
-          },
-        });
-  
-        res.status(200).send("unliked");
-      } else {
-        res.status(400).send("like does not exist");
-      }
-    } catch (err) {
-      res.status(400).send("Some error occured");
-    }
-});
-  
-  router.post("/", async (req, res, next) => {
-    try {
-        const { favorite, placeId, userId } = req.body;
-        console.log(favorite);
 
-        let newLike = Like.build({
-            favorite: favorite,
-            placeId: placeId,
-            userId: userId,
-        });
-
-        newLike
-            .save()
-            .then((like) => {
-            return res.status(200).json(like);
-            })
-            .catch((err) => {
-            return res.status(400).json("Error occured");
-            });
-    } catch (err) {
-      res.status(400).send("Some error occured");
+      res.status(200).send("unliked");
+    } else {
+      res.status(400).send("like does not exist");
     }
+  } catch (err) {
+    res.status(400).send("Some error occured");
+  }
 });
-  
-  module.exports = router;
-  
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { favorite, placeId, userId } = req.body;
+    console.log(favorite);
+
+    let newLike = await Like.create({
+      favorite: favorite,
+      placeId: placeId,
+      userId: userId,
+    });
+
+    res.status(200).json(newLike);
+  } catch (err) {
+    res.status(400).send("Some error occured");
+  }
+});
+
+module.exports = router;
