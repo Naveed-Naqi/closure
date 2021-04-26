6e2e4d39a5cb683f08b47efc7cc9b056e937da63
Naveed Naqi <nnaqi5534@gmail.com>
2021-04-26 12:03:04 -0400

    finished image uplaoding

--- client/src/components/info/ImageUpload.js
+++ client/src/components/info/ImageUpload.js
@@ -38,15 +38,6 @@ import ReplayIcon from "@material-ui/icons/Replay";
 //Tabs
 import { withStyles } from "@material-ui/core/styles";
 
-// const imageGallery = [
-//   "https://raw.githubusercontent.com/dxyang/StyleTransfer/master/style_imgs/mosaic.jpg",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg",
-//   "https://raw.githubusercontent.com/ShafeenTejani/fast-style-transfer/master/examples/dora-maar-picasso.jpg",
-//   "https://pbs.twimg.com/profile_images/925531519858257920/IyYLHp-u_400x400.jpg",
-//   "https://raw.githubusercontent.com/ShafeenTejani/fast-style-transfer/master/examples/dog.jpg",
-//   "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"
-// ];
-
 const styles = (theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
@@ -119,7 +110,6 @@ class ImageUploadCard extends React.Component {
   };
 
   handleUploadClick = (event) => {
-    console.log();
     var file = event.target.files[0];
     const reader = new FileReader();
     var url = reader.readAsDataURL(file);
@@ -129,7 +119,6 @@ class ImageUploadCard extends React.Component {
         selectedFile: [reader.result],
       });
     }.bind(this);
-    console.log(url); // Would see a path?
 
     this.props.updateSelectedImage(event.target.files[0]);
 
@@ -139,18 +128,6 @@ class ImageUploadCard extends React.Component {
       imageUploaded: 1,
     });
   };
-
-  //   handleSearchClick = event => {
-  //     this.setState({
-  //       mainState: "search"
-  //     });
-  //   };
-
-  //   handleGalleryClick = event => {
-  //     this.setState({
-  //       mainState: "gallery"
-  //     });
-  //   };
 
   renderInitialState() {
     const { classes, theme } = this.props;
@@ -173,118 +150,37 @@ class ImageUploadCard extends React.Component {
                 <AddPhotoAlternateIcon />
               </Fab>
             </label>
-            {/* <Fab className={classes.button} onClick={this.handleSearchClick}>
-              <SearchIcon />
-            </Fab> */}
-            {/* <Fab className={classes.button} onClick={this.handleGalleryClick}>
-              <CollectionsIcon />
-            </Fab> */}
           </Grid>
         </CardContent>
       </React.Fragment>
     );
   }
 
-  //   handleSearchURL = event => {
-  //     console.log();
-  //     var file = event.target.files[0];
-  //     var reader = new FileReader();
-  //     var url = reader.readAsDataURL(file);
-
-  //     reader.onloadend = function(e) {
-  //       this.setState({
-  //         selectedFile: [reader.result]
-  //       });
-  //     }.bind(this);
-  //     console.log(url); // Would see a path?
-
-  //     this.setState({
-  //       selectedFile: event.target.files[0],
-  //       imageUploaded: 1
-  //     });
-  //   };
-
-  //   handleImageSearch(url) {
-  //     var filename = url.substring(url.lastIndexOf("/") + 1);
-  //     console.log(filename);
-  //     this.setState({
-  //       mainState: "uploaded",
-  //       imageUploaded: true,
-  //       selectedFile: url,
-  //       fileReader: undefined,
-  //       filename: filename
-  //     });
-  //   }
-
-  handleSeachClose = (event) => {
+  handleAvatarClick(value) {
+    var filename = value.url.substring(value.url.lastIndexOf("/") + 1);
+    console.log(filename);
     this.setState({
-      mainState: "initial",
+      mainState: "uploaded",
+      imageUploaded: true,
+      selectedFile: value.url,
+      fileReader: undefined,
+      filename: filename,
     });
-  };
-
-  renderSearchState() {
-    const { classes } = this.props;
-
-    return (
-      <Paper className={classes.searchRoot} elevation={1}>
-        <InputBase className={classes.searchInput} placeholder="Image URL" />
-        <IconButton
-          className={classes.button}
-          aria-label="Search"
-          onClick={this.handleImageSearch}
-        >
-          <SearchIcon />
-        </IconButton>
-        {/* <Divider className={classes.searchDivider} />
-        <IconButton
-          color="primary"
-          className={classes.secondaryButton}
-          aria-label="Close"
-          onClick={this.handleSeachClose}
-        >
-          <CloseIcon />
-        </IconButton> */}
-      </Paper>
-    );
   }
 
-  //   handleAvatarClick(value) {
-  //     var filename = value.url.substring(value.url.lastIndexOf("/") + 1);
-  //     console.log(filename);
-  //     this.setState({
-  //       mainState: "uploaded",
-  //       imageUploaded: true,
-  //       selectedFile: value.url,
-  //       fileReader: undefined,
-  //       filename: filename
-  //     });
-  //   }
-
   renderGalleryState() {
-    // const { classes } = this.props;
-    // const listItems = this.props.imageGallery.map(url => (
-    //   <div
-    //     onClick={value => this.handleAvatarClick({ url })}
-    //     style={{
-    //       padding: "5px 5px 5px 5px",
-    //       cursor: "pointer"
-    //     }}
-    //   >
-    //     <Avatar src={url} />
-    //   </div>
-    // ));
-
-    /*const listItems = this.props.imageGallery.map(url => (
+    const { classes } = this.props;
+    const listItems = this.props.imageGallery.map((url) => (
       <div
-        onClick={value => this.handleAvatarClick({ url })}
+        onClick={(value) => this.handleAvatarClick({ url })}
         style={{
           padding: "5px 5px 5px 5px",
-          cursor: "pointer"
+          cursor: "pointer",
         }}
       >
-        <Avatar shape="square" size={100} src={url} />
+        <Avatar src={url} />
       </div>
-    ));*/
+    ));
 
     return (
       <React.Fragment>
@@ -336,9 +232,6 @@ class ImageUploadCard extends React.Component {
         <div className={classes.root}>
           <Card className={this.props.cardName}>
             {(this.state.mainState == "initial" && this.renderInitialState()) ||
-              (this.state.mainState == "search" && this.renderSearchState()) ||
-              //   (this.state.mainState == "gallery" &&
-              //     this.renderGalleryState()) ||
               (this.state.mainState == "uploaded" &&
                 this.renderUploadedState())}
           </Card>
