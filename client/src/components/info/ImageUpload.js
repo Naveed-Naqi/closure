b3656092daea9e6522226cd8d9b8abb732a511b8
Naveed Naqi <nnaqi5534@gmail.com>
2021-04-26 20:40:19 -0400

    removed a bunch more warnings

--- client/src/components/info/ImageUpload.js
+++ client/src/components/info/ImageUpload.js
@@ -3,36 +3,21 @@ import React from "react";
 //Card
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import CardMedia from "@material-ui/core/CardMedia";
 
 import Fab from "@material-ui/core/Fab";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 
 import red from "@material-ui/core/colors/red";
-import pink from "@material-ui/core/colors/pink";
 import blue from "@material-ui/core/colors/blue";
 
-import Icon from "@material-ui/core/Icon";
-import PageviewIcon from "@material-ui/icons/Pageview";
-import SearchIcon from "@material-ui/icons/Search";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
-import CollectionsIcon from "@material-ui/icons/Collections";
-
-import Typography from "@material-ui/core/Typography";
-import Popover from "@material-ui/core/Popover";
 
 // Search
-import Paper from "@material-ui/core/Paper";
-import InputBase from "@material-ui/core/InputBase";
-import Divider from "@material-ui/core/Divider";
+
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import CloseIcon from "@material-ui/icons/Close";
+
 import ReplayIcon from "@material-ui/icons/Replay";
 
 //Tabs
